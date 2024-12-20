8dd66813d956c8fa41ee46211b0dfa3186a756aa
Nick Ilin <ninikitos90@gmail.com>
2024-12-20 00:02:02 -0500

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# dj_website
+# Interactive website for a DJ
+
+### Key features
+1. Scroll animations using GSAP;
+2. Music player on a button press;
+3. Parses TikTok stream once it's started.
+
+### Web Framework
+
+- Django
---
+++ dj_website/settings/base.py
@@ -0,0 +1,155 @@
+from decouple import Csv, config
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# ==============================================================================
+# CORE SETTINGS
+# ==============================================================================
+
+SECRET_KEY = config("SECRET_KEY", default='django-insecure$dj_website.settings.base')
+
+DEBUG = config("DEBUG", default=True, cast=bool)
+
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="127.0.0.1,localhost", cast=Csv())
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+
+    "dj_website.apps.main",
+]
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+ROOT_URLCONF = "dj_website.urls"
+
+INTERNAL_IPS = ["127.0.0.1"]
+
+WSGI_APPLICATION = "dj_website.wsgi.application"
+
+# ==============================================================================
+# MIDDLEWARE SETTINGS
+# ==============================================================================
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+# ==============================================================================
+# TEMPLATES SETTINGS
+# ==============================================================================
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+# ==============================================================================
+# DATABASES SETTINGS
+# ==============================================================================
+
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         default=config("DATABASE_URL", default="postgres://simple:simple@localhost:5432/simple"),
+#         conn_max_age=600,
+#     )
+# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# ==============================================================================
+# AUTHENTICATION AND AUTHORIZATION SETTINGS
+# ==============================================================================
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+# ==============================================================================
+# I18N AND L10N SETTINGS
+# ==============================================================================
+
+LANGUAGE_CODE = config("LANGUAGE_CODE", default="en-us")
+
+TIME_ZONE = config("TIME_ZONE", default="UTC")
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+# ==============================================================================
+# STATIC FILES SETTINGS
+# ==============================================================================
+
+STATIC_URL = "/static/"
+
+STATIC_ROOT = BASE_DIR.parent.parent / "static"
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+)
+
+# ==============================================================================
+# MEDIA FILES SETTINGS
+# ==============================================================================
+
+MEDIA_URL = "/media/"
+
+MEDIA_ROOT = BASE_DIR.parent.parent / "media"
+
+# ==============================================================================
+# THIRD-PARTY SETTINGS
+# ==============================================================================
+
+# TBD
+
+# ==============================================================================
+# FIRST-PARTY SETTINGS
+# ==============================================================================
+
+# SIMPLE_ENVIRONMENT = config("DJ_WEBSITE_ENVIRONMENT", default="base")
